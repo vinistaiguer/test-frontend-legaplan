27b283b2a095833200d68eaf4c263a059fba1afa
vinicius.santos <vinicius.santos@icolabora.com>
2024-09-22 10:51:29 -0300

    changed

---
+++ .next/static/chunks/app/page-33bf4c603a286dd6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{7323:function(n,e,u){Promise.resolve().then(u.bind(u,4225))}},function(n){n.O(0,[911,472,225,971,23,744],function(){return n(n.s=7323)}),_N_E=n.O()}]);
---
+++ .next/static/chunks/app/task-manager/page-57db16f24725d412.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[387],{5984:function(n,e,u){Promise.resolve().then(u.bind(u,4225))}},function(n){n.O(0,[911,472,225,971,23,744],function(){return n(n.s=5984)}),_N_E=n.O()}]);
